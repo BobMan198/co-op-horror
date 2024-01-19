2ff1589569c59b68090d5f033fb1ef5e46256db2
BobMan198 <ashernewhouse@gmail.com>
2024-01-19 13:40:27 -0600

    1

--- Assets/Scripts/Monster/RecordPlayers.cs
+++ Assets/Scripts/Monster/RecordPlayers.cs
@@ -1,16 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Dissonance.Audio.Capture;
+using JetBrains.Annotations;
+using NAudio.Wave;
 using UnityEngine;
 
-public class RecordPlayers : MonoBehaviour
+public class RecordPlayers : MonoBehaviour, IMicrophoneCapture
 {
-    // Start is called before the first frame update
+    public bool IsRecording => throw new NotImplementedException();
+
+    public string Device => throw new NotImplementedException();
+
+    public TimeSpan Latency => throw new NotImplementedException();
+
+    public WaveFormat StartCapture([CanBeNull] string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void StopCapture()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Subscribe([NotNull] IMicrophoneSubscriber listener)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Unsubscribe([NotNull] IMicrophoneSubscriber listener)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool UpdateSubscribers()
+    {
+        throw new NotImplementedException();
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
