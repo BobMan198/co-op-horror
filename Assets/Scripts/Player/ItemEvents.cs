dcc006d67fe54f5b6685cf2347e4612d7c61e207
BobMan198 <ashernewhouse@gmail.com>
2024-04-27 11:50:58 -0500

    Live camera is now synced

--- Assets/Scripts/Player/ItemEvents.cs
+++ Assets/Scripts/Player/ItemEvents.cs
@@ -1,54 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class ItemEvents : MonoBehaviour
+public class ItemEvents : NetworkBehaviour
 {
     public PickupItem itemInHand;
 
     [SerializeField]
     private GameObject cameraItem;
-
-    private void Start()
-    {
-
-    }
     private void Update()
     {
         if(itemInHand != null)
         {
             if(itemInHand.ItemName == "Camera")
             {
-                cameraItem = GameObject.FindObjectOfType<LiveCamera>().gameObject;
-                var camera = cameraItem.GetComponentInChildren<Camera>();
-
                 if (Input.GetMouseButton(1))
                 {
-                    float zoomTimer =+ Time.deltaTime;
-                    float zoom = zoomTimer * 45;
-                    if (camera.focalLength >= 120)
-                    {
-                        return;
-                    }
-                    else
-                    {
-                        camera.focalLength += zoom;
-                    }
+                    ZoomCameraServerRpc();
                 }
                 else
                 {
-                    float zoomTimer =- Time.deltaTime;
-                    float zoom = zoomTimer * 45;
-                    if (camera.focalLength <= 50)
-                    {
-                        return;
-                    }
-                    else
-                    {
-                        camera.focalLength += zoom;
-                    }
+                    UnZoomCameraServerRpc();
                 }
             }
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void ZoomCameraServerRpc()
+    {
+        ZoomCameraClientRpc();
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void UnZoomCameraServerRpc()
+    {
+        UnZoomCameraClientRpc();
+    }
+
+    [ClientRpc]
+    private void ZoomCameraClientRpc()
+    {
+        cameraItem = FindObjectOfType<LiveCamera>().gameObject;
+        var camera = cameraItem.GetComponentInChildren<Camera>();
+
+        float zoomTimer = +Time.deltaTime;
+        float zoom = zoomTimer * 45;
+        if (camera.focalLength >= 120)
+        {
+            return;
+        }
+        else
+        {
+            camera.focalLength += zoom;
+        }
+    }
+
+    [ClientRpc]
+    private void UnZoomCameraClientRpc()
+    {
+        cameraItem = FindObjectOfType<LiveCamera>().gameObject;
+        var camera = cameraItem.GetComponentInChildren<Camera>();
+
+        float zoomTimer = -Time.deltaTime;
+        float zoom = zoomTimer * 45;
+        if (camera.focalLength <= 50)
+        {
+            return;
+        }
+        else
+        {
+            camera.focalLength += zoom;
         }
     }
 }
--- Assets/Scripts/Player/PlayerRespawn.cs
+++ Assets/Scripts/Player/PlayerRespawn.cs
@@ -5,15 +5,18 @@ using System.Globalization;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Unity.Netcode;
+using Unity.VisualScripting;
 
 public class PlayerRespawn : NetworkBehaviour
 {
 
     private PlayerMovement pm;
     private LayerMask playerlayer = 8;
+    private GameRunner gameRunner;
     private void Start()
     {
         pm = GetComponent<PlayerMovement>();
+        gameRunner = FindObjectOfType<GameRunner>();
     }
 
     private void Update()   
@@ -23,38 +26,60 @@ public class PlayerRespawn : NetworkBehaviour
 
         if (sceneName == "HQ")
         {
-            if (IsLocalPlayer)
+            if (gameRunner.alivePlayers.Count >= gameRunner.playersLoadedIn.Count)
             {
-                var dissonance = FindObjectOfType<DissonanceComms>();
-                var voiceChat = dissonance.FindPlayer(dissonance.LocalPlayerName);
-
-                gameObject.tag = "Player";
-                gameObject.layer = playerlayer;
-                dissonance.IsMuted = false;
-                pm.enabled = true;
-                pm.playerHealth = 100;
-                pm.playerCamera.gameObject.SetActive(true);
-                pm.playerCamera.enabled = true;
-                pm.fadeBlack.gameObject.SetActive(false);
-                pm.spectatorCamera.gameObject.SetActive(false);
-                pm.spectatorCamera.transform.SetParent(transform);
-                pm.controller.enabled = true;
-                pm.audioListener.enabled = true;
-                pm.playerStaminaUI.SetActive(true);
-                pm.playerItemHolder.SetActive(true);
-
+                RespawnPlayersServerRpc();
             }
-
-            if(pm.enabled == false)
-            {
-                pm.enabled = true;
-            }
-
         }
         else
         {
             return;
         }
 
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void RespawnPlayersServerRpc()
+    {
+        var dissonance = FindObjectOfType<DissonanceComms>();
+        var voiceChat = dissonance.FindPlayer(dissonance.LocalPlayerName);
+
+        gameObject.tag = "Player";
+        gameObject.layer = playerlayer;
+        dissonance.IsMuted = false;
+        pm.enabled = true;
+        pm.playerHealth = 100;
+        pm.playerCamera.gameObject.SetActive(true);
+        pm.playerCamera.enabled = true;
+        pm.fadeBlack.gameObject.SetActive(false);
+        pm.spectatorCamera.gameObject.SetActive(false);
+        pm.spectatorCamera.transform.SetParent(transform);
+        pm.controller.enabled = true;
+        pm.audioListener.enabled = true;
+        pm.playerStaminaUI.SetActive(true);
+        pm.playerItemHolder.SetActive(true);
+        RespawnPlayersClientRpc();
+    }
+
+    [ClientRpc]
+    private void RespawnPlayersClientRpc()
+    {
+        var dissonance = FindObjectOfType<DissonanceComms>();
+        var voiceChat = dissonance.FindPlayer(dissonance.LocalPlayerName);
+
+        gameObject.tag = "Player";
+        gameObject.layer = playerlayer;
+        dissonance.IsMuted = false;
+        pm.enabled = true;
+        pm.playerHealth = 100;
+        pm.playerCamera.gameObject.SetActive(true);
+        pm.playerCamera.enabled = true;
+        pm.fadeBlack.gameObject.SetActive(false);
+        pm.spectatorCamera.gameObject.SetActive(false);
+        pm.spectatorCamera.transform.SetParent(transform);
+        pm.controller.enabled = true;
+        pm.audioListener.enabled = true;
+        pm.playerStaminaUI.SetActive(true);
+        pm.playerItemHolder.SetActive(true);
     }
 }
